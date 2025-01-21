081eb2332a06c54662d0413cca232436743b954f
ABCD-rgb <agdelmo1@up.edu.ph>
2025-01-21 15:38:43 +0800

    chore: Dashboard shows correct username

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <div className="flex justify-between items-center mb-8">
             <div className="font-bold text-2xl text-center">
-                Hi Arawela!
+                Hi {localStorage.getItem("username")}!
             </div>
             <button className="btn btn-error" onClick={() => {window.location.href = "/";}}>
                 Logout
